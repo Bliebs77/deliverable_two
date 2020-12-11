2d456d6d41bce7818520c001291a0d187f7e30ef
barry <b_lieberman@hotmail.com>
2020-12-11 09:04:06 -0500

    This is the first commit for deliverable 2

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let rain = prompt("How many inches of rain fell");
+
+let string = "*";
+
+console.log ()
+
+
+let fert = prompt("Did you use fertilizer?");
+
+let fert_type = prompt ("Did you use premium or regular fertilizer?");
+
+
+
+
+
+
